39c7a1d4bb38d60d4297c4be18f93a2c94716aa2
Van-Dyck Adanuty <adanutyv@msu.edu>
2026-07-14 21:12:54 -0400

    Beta array, gain ceiling, bin smoothing

--- generator.py
+++ generator.py
@@ -10,14 +10,86 @@ H_transfer_function = fft(room_impulse_response)
 
 
 #Calculate the inverse filter with Kirkeby Regularization
-beta = 0.005
-H_inv_transfer_function = np.conj(H_transfer_function) / (np.abs(H_transfer_function)**2 + beta)
+# --- 0. FREQUENCY-DEPENDENT REGULARIZATION (Smooth Ramps) ---
+N = len(H_transfer_function)
+beta_array = np.full(N, 0.005)
+
+# --- Define Frequency Boundaries ---
+sub_bass_start_idx = int(np.round(40 * (N / sample_rate)))
+sub_bass_end_idx = int(np.round(80 * (N / sample_rate)))
+
+high_freq_start_idx = int(np.round(16000 * (N / sample_rate)))
+high_freq_end_idx = int(np.round(18000 * (N / sample_rate)))
+
+
+beta_array[0:sub_bass_start_idx] = 0.1
+
+# Smooth ramp from 40 Hz down to 80 Hz
+sub_bass_ramp_length = sub_bass_end_idx - sub_bass_start_idx
+beta_array[sub_bass_start_idx:sub_bass_end_idx] = np.linspace(0.1, 0.005, sub_bass_ramp_length)
+
+
+# Smooth ramp from 16 kHz up to 18 kHz
+high_freq_ramp_length = high_freq_end_idx - high_freq_start_idx
+beta_array[high_freq_start_idx:high_freq_end_idx] = np.linspace(0.005, 0.1, high_freq_ramp_length)
+
+# Hard penalty from 18 kHz to Nyquist
+beta_array[high_freq_end_idx:N // 2] = 0.1
+
+
+# Mirror Sub-Bass
+beta_array[N - sub_bass_start_idx:N] = 0.1
+beta_array[N - sub_bass_end_idx:N - sub_bass_start_idx] = np.linspace(0.005, 0.1, sub_bass_ramp_length)
+
+# Mirror High-Frequency
+beta_array[N // 2:N - high_freq_end_idx] = 0.1
+beta_array[N - high_freq_end_idx:N - high_freq_start_idx] = np.linspace(0.1, 0.005, high_freq_ramp_length)
+
+
+H_inv_transfer_function = np.conj(H_transfer_function) / (np.abs(H_transfer_function)**2 + beta_array)
+
+gain_ceiling_value = 8 #dB
+gain_ceiling_value_linear = 10**(gain_ceiling_value/20)
+
+inv_magnitude = np.abs(H_inv_transfer_function)
+inv_phase = np.angle(H_inv_transfer_function)
+
+clipped_inv_magnitude = np.clip(inv_magnitude, 0, gain_ceiling_value_linear)
+
+
+
+magnitude_smoothed = np.copy(clipped_inv_magnitude)
+
+N = len(clipped_inv_magnitude)
+
+#For a schoeder frequency 500
+starting_index = int(np.round(500 * (N/sample_rate)))
+
+for i in range(starting_index, N//2):
+    center_frequency = i * (sample_rate/N)
+    lower_frequency = center_frequency * 2**(-1/6)
+    higher_frequency = center_frequency * 2**(1/6)
+
+    lower_frequency_index = int(np.round(lower_frequency *  (N/sample_rate)))
+    higher_frequency_index = int(np.round(higher_frequency *  (N/sample_rate)))
+
+
+    current_window = clipped_inv_magnitude[lower_frequency_index:higher_frequency_index]
+    
+    if len(current_window) > 0:
+        smoothed_val = np.mean(current_window)
+        magnitude_smoothed[i] = smoothed_val
+        magnitude_smoothed[N-i] = smoothed_val
+
+
+H_inv_clipped = magnitude_smoothed * np.exp(1j * inv_phase)
+
 
 
 # Change to time domain
-correction_filter = np.real(ifft(H_inv_transfer_function))
+correction_filter = np.real(ifft(H_inv_clipped))
 
-N = len(correction_filter)
+
 
 shift_amount = N//2
 
@@ -59,7 +131,7 @@ stereo_filter = np.column_stack((final_filter, final_filter)).astype(np.float32)
 wavfile.write("data/correction_filter.wav", sample_rate, stereo_filter)
 
 latency = pre_len
-print(f"setLatencySamples({latency});   // paste into prepareToPlay")
+print(f"setLatencySamples({latency}); ")
 
 
 #  Plot the filter
@@ -76,4 +148,14 @@ plt.ylabel("Magnitude (dB)")
 plt.xscale('log')
 plt.xlim(20, 20000)
 plt.grid(True, which="both", ls="-", color='0.8')
+plt.show()
+
+transfer_function_db = 20 * np.log10(np.abs(H_transfer_function))
+plt.figure(figsize=(10,4))
+plt.plot(transfer_function_db)
+plt.xlabel("Freq(Hz)")
+plt.ylabel("Magnitude(dB)")
+plt.xscale('log')
+plt.title("Transfer function")
+plt.grid(True, which="both")
 plt.show()
